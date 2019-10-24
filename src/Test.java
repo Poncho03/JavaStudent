664efe5a59b5fcdbea0282cc7b5da15b559dabb2
Luis Alfonso Gonzalez <luis030199@gmail.com>
2019-10-24 17:20:44 -0500

    Se agregó método de números perfectos

--- src/Test.java
+++ src/Test.java
@@ -3,6 +3,23 @@ public class Test {
     static int SumDosNum(int n1, int n2){
         return n1+n2;
     }
+    
+    static void NumPer(int n, int div, int c){
+        while(div!=n){
+            if(n%div==0){
+                c=c+div;
+                div++;
+            }
+            else{
+                div++;
+            }   
+        }
+        if(c==n)
+            Tools.imprimir(n+" es perfecto");
+        else
+            Tools.imprimir(n+ " no es perfecto");
+    }
+    
     public static void main(String[]args){
         Tools.imprimir("La suma de los numeros es: "+SumDosNum(Tools.LeerInt("Ingresa un numero"),Tools.LeerInt("Ingresa otro numero")));
     }
